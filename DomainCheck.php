aa98a22ea90c999e9ea17dc1bbbd838200bc1275
amodsal <amod83@hotmail.com>
2018-02-16 02:15:48 -0500

    design

--- DomainCheck.php
+++ DomainCheck.php
@@ -1,43 +1,90 @@
-<?php
-    
-//include the required files
-require_once('includes/config.php');
-require_once('classes/reseller_api.php');
+<!DOCTYPE html>
+<html lang="en">
 
-//initialise the base reseller_api object
-$reseller_api = new reseller_api();
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="title" content="Domain">
+   
+    <link rel="shortcut icon" href="assets/img/favicon.png">
 
-//construct the request data
-$request = array(
-	'DomainNames' => array(
-		$_POST['domainname']
-	)
-);
+    <title>Domain</title>
 
-//send the request
-$response = $reseller_api->call('DomainCheck', $request);
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-theme.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/animations.css">
 
-if (!is_soap_fault($response)) {
+    <!-- siimple style -->
+    <link href="assets/css/style.css" rel="stylesheet">
 
-	//Successfully checked the availability of the domains
-	if (isset($response->APIResponse->AvailabilityList)) {
-		echo $response->APIResponse->AvailabilityList[0]->Item . ' - is ';
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
 
-		if (!$response->APIResponse->AvailabilityList[0]->Available) {
-			echo 'not ';
-		}
+<body>
+    <div id="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <?php
 
-		echo 'available<br />';
-	} else {
-		echo 'The following error(s) occurred:<br />';
+                    //include the required files
+                    require_once('includes/config.php');
+                    require_once('classes/reseller_api.php');
 
-		foreach ($response->APIResponse->Errors as $error) {
-			echo $error->Item . ' - ' . $error->Message . '<br />';
-		}
-	}
-} else {
+                    //initialise the base reseller_api object
+                    $reseller_api = new reseller_api();
 
-	//SoapFault
-	echo 'Error occurred while sending request: ' . $response->getMessage();
-}
-?>
+                    //construct the request data
+                    $request = array(
+                        'DomainNames' => array(
+                            $_POST['domainname']
+                        )
+                    );
+
+                    //send the request
+                    $response = $reseller_api->call('DomainCheck', $request);
+
+                    if (!is_soap_fault($response)) {
+
+                        //Successfully checked the availability of the domains
+                        if (isset($response->APIResponse->AvailabilityList)) {
+                            echo '<h2 class="subtitle">'. $response->APIResponse->AvailabilityList[0]->Item . ' - is ';
+
+                            if (!$response->APIResponse->AvailabilityList[0]->Available) {
+                                echo 'not ';
+                            }
+
+                            echo 'available</h2><br />';
+                        } else {
+                            echo '<h2 class="subtitle">The following error(s) occurred:<br />';
+
+                            foreach ($response->APIResponse->Errors as $error) {
+                                echo $error->Item . ' - ' . $error->Message . '</h2><br />';
+                            }
+                        }
+                    } else {
+
+                        //SoapFault
+                        echo '<h2 class="subtitle">Error occurred while sending request: ' . $response->getMessage() . '</h2>';
+                    }
+                    ?>
+                </div>
+                </div>
+        </div>
+    </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="http://downloads.mailchimp.com/js/jquery.form-n-validate.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#wrapper').height($(document).height());
+        });
+    </script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,31 +1,78 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Test App</title>
-        <meta name="description" content="Harmattan Web Hosting">
-        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
-    <style>
-        h1 {
-            font-family: 'Baloo Bhai', cursive;
-            font-size: 37px;
-            text-align: center;
-        }
-        p {
-            font-family: 'Roboto', sans-serif;
-            font-size: 14px;
-            text-align: center;
-        }
-    </style>
-    
-    </head>
-    <body>
-        <h1>This is a test</h1>
-        <p>by Ahmed</p>
-        <form action="DomainCheck.php" method="post">
-            Domain Name: <input type="text" name="domainname" /><br />
-            <input type="submit" name="submit" value="Search" />
-        </form>
-    </body>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="title" content="Domain">
+   
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+
+    <title>Domain</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-theme.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/animations.css">
+
+    <!-- siimple style -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <br/>
+                    <br/>
+                    <h2 class="subtitle">Search for a domain name</h2>
+                    <br/>
+             
+                    <form action="DomainCheck.php" method="post" class="form-inline validate signup">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="domainname" id="exampleInputEmail1" placeholder="Find a domain">
+                        </div>
+                        <input type="submit" name="submit" value="Search" class="btn btn-theme">
+                    </form>
+                    <br/>
+                </div>
+                <div class="col-sm-12 align-center">
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="#" class="icoGit" title="Github"><i class="fa fa-github"></i></a>
+                        </li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-lg-offset-3">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="http://downloads.mailchimp.com/js/jquery.form-n-validate.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#wrapper').height($(document).height());
+        });
+    </script>
+
+</body>
+
+</html>
